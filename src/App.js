c82d06be1f9399a1acc0152cc58162bcb6f67aa2
lk-mmt <lakshmikanth.am@go-mmt.com>
2021-02-22 01:18:32 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello my awesome react app!!!</p>
+        <p>Hello my awesome react app!!! :)</p>
       </header>
     </div>
   );
